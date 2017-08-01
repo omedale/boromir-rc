3d199f74bea837bed5aeeeb74612aafcba7f0e56
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-2.local>
2017-08-01 12:50:47 +0100

    feat(marketplace): multivendor - ensure the template for signup is showing - ensure events and helpers are set for signup and update vendordetails [Footer #148984919]

--- client/modules/accounts/templates/signUp/signUp.js
+++ client/modules/accounts/templates/signUp/signUp.js
@@ -1,3 +1,7 @@
+/* global Session:true */
+/* global Meteor:true */
+/* global LoginFormValidation:true */
+
 import { LoginFormSharedHelpers } from "/client/modules/accounts/helpers";
 import { Template } from "meteor/templating";
 
@@ -10,6 +14,7 @@ Template.loginFormSignUpView.onCreated(() => {
   template.uniqueId = Random.id();
   template.formMessages = new ReactiveVar({});
   template.type = "signUp";
+  Session.set("userType", $(".form-radio-input").val());
 });
 
 /**
@@ -30,20 +35,31 @@ Template.loginFormSignUpView.events({
   "submit form": function (event, template) {
     event.preventDefault();
 
-    // var usernameInput = template.$(".login-input--username");
+    const usernameInput = template.$(".login-input-username");
     const emailInput = template.$(".login-input-email");
     const passwordInput = template.$(".login-input-password");
 
+    const username = usernameInput.val().trim();
     const email = emailInput.val().trim();
     const password = passwordInput.val().trim();
 
+    const validatedUsername = LoginFormValidation.username(username);
     const validatedEmail = LoginFormValidation.email(email);
     const validatedPassword = LoginFormValidation.password(password);
 
     const templateInstance = Template.instance();
     const errors = {};
 
+    let vendorDetails = {};
+    let vendorName = "";
+    let vendorPhone = "";
+    let vendorAddr = "";
+
     templateInstance.formMessages.set({});
+
+    if (validatedUsername !== true) {
+      errors.username = validatedUsername;
+    }
 
     if (validatedEmail !== true) {
       errors.email = validatedEmail;
@@ -51,6 +67,34 @@ Template.loginFormSignUpView.events({
 
     if (validatedPassword !== true) {
       errors.password = validatedPassword;
+    }
+
+    if (Session.get("userType") === "vendor") {
+      vendorName = template.$(".login-input-vendorName").val();
+      vendorPhone = template.$(".login-input-vendorPhone").val();
+      vendorAddr = template.$(".login-input-vendorAddr").val();
+
+      if (!vendorName || !/\w+/gi.test(vendorName)) {
+        errors.vendorName = { i18nKeyReason: "Invalid vendor name", reason: "Invalid vendor name" };
+      }
+
+      if (!(/^['+']?[0-9]{6,14}$/).test(vendorPhone)) {
+        errors.vendorPhone = { i18nKeyReason: "Invalid phone number", reason: "Invalid phone number" };
+      }
+
+      if (!vendorAddr || !/[\w+\s\/,?]+(\.)?/.test(vendorAddr)) {
+        errors.vendorAddr = { i18nKeyReason: "Invalid address", reason: "Invalid address" };
+      }
+
+      if (vendorName && vendorPhone && vendorAddr) {
+        vendorDetails = {
+          vendorName: vendorName,
+          vendorPhone: vendorPhone,
+          vendorAddr: vendorAddr
+        };
+      } else {
+        vendorDetails = null;
+      }
     }
 
     if ($.isEmptyObject(errors) === false) {
@@ -63,11 +107,19 @@ Template.loginFormSignUpView.events({
 
     const newUserData = {
       // username: username,
+      username: username,
       email: email,
-      password: password
+      password: password,
+      profile: {
+        vendorDetails: vendorDetails
+      },
+      userType: Session.get("userType") || "buyer"
     };
 
     Accounts.createUser(newUserData, function (error) {
+      if (Session.get("userType") === "vendor") {
+        Meteor.call("accounts/updateVendorDetails", vendorDetails);
+      }
       if (error) {
         // Show some error message
         templateInstance.formMessages.set({
@@ -77,5 +129,9 @@ Template.loginFormSignUpView.events({
         // Close dropdown or navigate to page
       }
     });
+  },
+  "change .form-radio-input": function (event) {
+    const userType = event.target.value.toString().toLowerCase();
+    Session.set("userType", userType);
   }
 });
