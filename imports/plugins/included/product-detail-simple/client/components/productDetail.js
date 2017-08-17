8b1fe76a4357c7d1b6faff63ffc113aac6a1ce34
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2017-08-17 10:11:04 +0100

    feat(digital-product): - implement add a digital product - configure a cloud storage for the product

--- imports/plugins/included/product-detail-simple/client/components/productDetail.js
+++ imports/plugins/included/product-detail-simple/client/components/productDetail.js
@@ -16,11 +16,26 @@ import {
 } from "./";
 import { AlertContainer } from "/imports/plugins/core/ui/client/containers";
 import { PublishContainer } from "/imports/plugins/core/revisions";
+import "./firebase/firebaseAPI";
 
 class ProductDetail extends Component {
-  get tags() {
+  constructor(props) {
+     super(props);
+     this.state = {
+      showDigital: false,
+      downloadUrl: "",
+      progressBar: false,
+       progressLevel: 0
+     };
+     this.onDigitalChange = this.onDigitalChange.bind(this);
+     this.isDigital = this.isDigital.bind(this);
+     this.onDigitalSaveClick = this.onDigitalSaveClick.bind(this);
+     this.fileUpload =  this.fileUpload.bind(this);
+   }
+    get tags() {
     return this.props.tags || [];
   }
+
 
   get product() {
     return this.props.product || {};
@@ -30,9 +45,22 @@ class ProductDetail extends Component {
     return this.props.editable;
   }
 
+  isDigitalProduct= () => {
+
+  }
+
   handleVisibilityChange = (event, isProductVisible) => {
     if (this.props.onProductFieldChange) {
       this.props.onProductFieldChange(this.product._id, "isVisible", isProductVisible);
+    }
+  }
+
+  handleDigitalProduct = (event) => {
+    const digitalProductChecked = document.getElementById('digitalProduct').checked;
+    if(digitalProductChecked){
+      this.setState({
+        digitalProductChecked : true
+      })
     }
   }
 
@@ -41,6 +69,16 @@ class ProductDetail extends Component {
       this.props.onDeleteProduct(this.product._id);
     }
   }
+  componentDidMount() {
+    const isDigital = this.props.product.isDigital;
+     const downloadUrl = this.props.product.downloadUrl;
+     if (isDigital) {
+       document.getElementById("digital").checked = true;
+       this.setState({ showDigital: true, downloadUrl });
+     }
+ 
+   }
+
 
   renderToolbar() {
     if (this.props.hasAdminPermission) {
@@ -73,6 +111,71 @@ class ProductDetail extends Component {
 
     return null;
   }
+
+  isDigital() {
+     const isChecked =  document.getElementById("digital").checked;
+     if (isChecked) {
+       this.setState({ showDigital: true });
+     } else {
+       this.setState({ showDigital: false });
+       this.props.onProductFieldChange(this.product._id, "isDigital", false);
+       this.props.onProductFieldChange(this.product._id, "downloadUrl", '');
+     }
+   }
+ 
+   onDigitalSaveClick() {
+     if (this.state.downloadUrl == '') {
+       Alerts.toast("Enter download link", "error");
+     } else {
+       this.props.onProductFieldChange(this.product._id, "downloadUrl", this.state.downloadUrl);
+       this.props.onProductFieldChange(this.product._id, "isDigital", true);
+       document.getElementById('digital').checked = true;
+       Alerts.toast("Download Link successfully saved", "success");
+     }
+   }
+   onDigitalChange(e) {
+     this.setState({ downloadUrl: e.target.value });
+   }
+ 
+   fileUpload() {
+     const file = document.getElementById("file").files[0];
+     if (!file) {
+       Alerts.toast("Select a file", "error");
+       return;
+     }
+     if (file.size / 1000000 > 100) {
+       Alerts.toast("File size more than 100 MB", "error");
+       return;
+     }
+     $(".save").prop("disabled", true);
+     const fileName = file.name;
+     const storageRef = firebase.storage().ref("files");
+     const spaceRef = storageRef.child(fileName);
+     const uploadTask = spaceRef.put(file);
+     Alerts.toast("Uploading File...", "success");
+     uploadTask.on("state_changed", (snapshot) => {
+       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+       this.setState({ progressLevel: progress });
+       switch (snapshot.state) {
+         case firebase.storage.TaskState.PAUSED: // or 'paused'
+           console.log('Upload is paused');
+           break;
+         case firebase.storage.TaskState.RUNNING: // or 'running'
+           this.setState({ progressBar: true });
+           break;
+       }
+     }, function(error) {
+       Alerts.toast("Error in uploading file", "error");
+     }, () => {
+       const downloadURL = uploadTask.snapshot.downloadURL;
+       Alerts.toast("Uploading Completed", "success");
+       $("#file").val("");
+       $(".save").prop("disabled", false);
+       this.setState({ downloadUrl: downloadURL, progressBar: false });
+       this.onDigitalSaveClick();
+     });
+  }
+
 
   render() {
     return (
@@ -148,6 +251,41 @@ class ProductDetail extends Component {
                   }}
                 />
               </div>
+
+              {this.props.hasAdminPermission && <div className="digitalProduct"  style={{ marginLeft: "20px" }}>
+                 <div className="checkbox">
+                   <h4><input id="digital" type="checkbox" value="" onChange={() => this.isDigital()} />Digital Product(Upload File or Enter Download Link)</h4>
+                 </div>
+               </div>}
+  
+               {this.state.showDigital &&
+                 <div className="digitalProduct">
+                   <div className="checkbox">
+                   <div className="input-group"  style={{ width: "100%" }}>
+                     <span className="input-group-addon">Url</span>
+                     <input type="text" className="form-control" id="url" value={this.state.downloadUrl}
+                       onChange={this.onDigitalChange} placeholder="Enter download url" />
+                        <span className="input-group-btn">
+                     <button id="save" className="form-control btn btn-success save" onClick={this.onDigitalSaveClick}>Save</button>
+                     </span>
+                      </div>
+                   </div>
+                    <div className="input-group"  style={{ width: "100%" }}>
+                     <span className="input-group-addon">File(Limit: 100MB)</span>
+                     <input type="file" className="form-control" id="file" />
+                        <span className="input-group-btn">
+                     <button id="upload" className="form-control btn btn-success save" onClick={this.fileUpload}>Upload</button>
+                     </span>
+                     </div>
+                     <br />
+                     {this.state.progressBar &&
+                     <div className="progress">
+                     <div className="progress-bar progress-bar-striped active" role="progressbar" style={{ width: `${this.state.progressLevel}%` }} >
+                    Uploading </div>
+                   </div> }
+       
+                 </div>
+               }
 
               <div className="pdp product-info">
                 <ProductField
